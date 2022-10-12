d57e8b58d66bbe2bfe3899c94d81d34e1d1379b7
PaulTA91 <parmst202caledonian.ac.uk>
2022-10-12 10:44:01 +0100

    finished

---
+++ js/animals.js
@@ -0,0 +1,21 @@
+function CurrentAnimal(props) {
+  return (
+    <div>
+      <p> This is a {props.name} </p>
+    </div>
+  );
+}
+
+function Animals() {
+  return (
+    <div>
+      <CurrentAnimal name="dog"></CurrentAnimal>
+      <CurrentAnimal name="cat"></CurrentAnimal>
+      <CurrentAnimal name="mouse"></CurrentAnimal>
+    </div>
+  );
+}
+
+const domContainer = document.querySelector("#animalContainer");
+const root = ReactDOM.createRoot(domContainer);
+root.render(<Animals />);
---
+++ js/basicComponent.js
@@ -0,0 +1,7 @@
+const BasicComponent = () => {
+  return <h1>Welcome Message!</h1>;
+};
+
+const domContainer = document.querySelector("#basicComponentContainer");
+const root = ReactDOM.createRoot(domContainer);
+root.render(<BasicComponent />);
